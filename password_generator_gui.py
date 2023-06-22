62cab96bcb651f6e846c59311a5d5f66062f2058
Nicholas Arsenault <nickarsenault18@gmail.com>
2023-06-22 00:12:03 -0400

    Design changes: Opens in the center of your screen. Larger window with larger text. Feedback wasn't displaying - does now. Fonts and sizes specified. Going to add other features next.

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -43,9 +43,26 @@ def save_password_to_history(password):
     with open(HISTORY_FILE, 'a') as file:
         file.write(f"{password},{expiration_date.strftime('%Y-%m-%d')}\n")
 
+# Function to fade the "Copied to clipboard" label
+def fade_copied_label():
+    copied_label.config(text="")
+
 # Create the main window
 window = tk.Tk()
 window.title("Password Generator")
+
+# Get screen width and height
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# Calculate the center position
+window_width = 600
+window_height = 600
+x = (screen_width - window_width) // 2
+y = (screen_height - window_height) // 2
+
+# Set the window geometry
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 # Function to generate a password and display it
 def generate_password_handler():
@@ -71,48 +88,56 @@ def generate_password_handler():
 
     # Copy the password to clipboard
     copy_to_clipboard(password)
-    messagebox.showinfo("Password Generated", "Password copied to clipboard.")
+    copied_label.config(text="Copied to clipboard!", fg="green")
 
 
 
 
 # Label for password length
-length_label = tk.Label(window, text="Password Length:")
-length_label.pack()
+length_label = tk.Label(window, text="Password Length:", font=("Arial", 18))
+length_label.pack(pady=10)
 
 # Entry field for password length
-length_entry = tk.Entry(window)
+length_entry = tk.Entry(window, font=("Arial", 18))
 length_entry.insert(0, "12")  # Set default length
 length_entry.pack()
 
 # Checkboxes for character types
 letters_var = tk.BooleanVar(value=True)
-letters_checkbox = tk.Checkbutton(window, text="Include Letters", variable=letters_var)
+letters_checkbox = tk.Checkbutton(window, text="Include Letters", variable=letters_var, font=("Arial", 18))
 letters_checkbox.pack()
 
 digits_var = tk.BooleanVar(value=True)
-digits_checkbox = tk.Checkbutton(window, text="Include Digits", variable=digits_var)
+digits_checkbox = tk.Checkbutton(window, text="Include Digits", variable=digits_var, font=("Arial", 18))
 digits_checkbox.pack()
 
 symbols_var = tk.BooleanVar(value=True)
-symbols_checkbox = tk.Checkbutton(window, text="Include Symbols", variable=symbols_var)
+symbols_checkbox = tk.Checkbutton(window, text="Include Symbols", variable=symbols_var, font=("Arial", 18))
 symbols_checkbox.pack()
 
 # Button to generate password
-generate_button = tk.Button(window, text="Generate", command=generate_password_handler)
-generate_button.pack()
+generate_button = tk.Button(window, text="Generate", command=generate_password_handler, font=("Arial", 18))
+generate_button.pack(pady=10)
 
 # Label to display generated password
-password_label = tk.Label(window, text="Generated Password: ")
+password_label = tk.Label(window, text="Generated Password:", font=("Arial", 18))
 password_label.pack()
 
 # Label for password strength score
-strength_label = tk.Label(window, text="Strength Score: ")
+strength_label = tk.Label(window, text="Strength Score:", font=("Arial", 18))
 strength_label.pack()
 
 # Label for password strength feedback
-feedback_label = tk.Label(window, text="Feedback: ")
+feedback_label = tk.Label(window, text="Feedback:", font=("Arial", 18))
 feedback_label.pack()
+
+# Label to display "Copied to clipboard" message
+copied_label = tk.Label(window, text="", font=("Arial", 12))
+copied_label.pack(pady=10)
+
+# Schedule the fading effect
+window.after(3000, fade_copied_label)
 
 # Run the Tkinter event loop
 window.mainloop()
+
